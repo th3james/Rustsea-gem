d7a35827953918e3ab68f3fb828bda427ece2669
James Cox-Morton <th3james@fastmail.fm>
2016-09-19 17:50:54 +0100

    remove unused function

--- ext/rustsea/rustsea.c
+++ ext/rustsea/rustsea.c
@@ -8,10 +8,6 @@ VALUE cget_cell_value(VALUE mod) {
   return rb_str_new_cstr(val);
 };
 
-static VALUE hello_world(VALUE mod) {
-  return rb_str_new2("Hello World");
-}
-
 void Init_rustsea() {
   VALUE mRustsea = rb_define_module("Rustsea");
   rb_define_singleton_method(mRustsea, "hello_world", cget_cell_value, 0);
